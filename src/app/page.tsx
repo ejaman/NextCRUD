12fced37096c90762cfe15848379654564e3f203
ejaman <leegm17@naver.com>
2024-03-24 17:45:48 +0900

    feat: check server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { Inter } from "next/font/google";
-import React from "react";
+import React, { useState } from "react";
+import os from "os"; // node APIs
 
+// Memo: app 폴더 하의 친구들은 기본적으로 서버 컴포넌트
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
-  let a;
+  // You're importing a component that needs useState.
+  // It only works in a Client Component but none of its parents are marked with "use client",
+  // so they're Server Components by default.
+  // const [ex, setEx] = useState();
+
+  // HTML을 보내기 때문에
+  // 따라서 콘솔이 터미널에만 뜸!
+  console.log(1);
+  console.log(os.hostname());
 
   return <div>first page</div>;
 }
+// Memo: 브라우저에서 제공해주는 api는 사용 불가, 대신 node 환경에서 제공해주는 node api 사용 가능
